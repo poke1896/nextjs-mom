01383d2272490b17d198e2cfa8ef42d785c7fcaa
poke1896 <adrian.host96@gmail.com>
2024-07-08 19:15:09 -0600

    4
    4

--- app/data.ts
+++ app/data.ts
@@ -7,8 +7,8 @@ const data = {
     description: 'Guarakay es una microempresa artesanal dedicada a la creación de joyería única, hecha con elementos naturales que ya han terminado su ciclo de vida, tales como alas de mariposa, plumas de aves, y hojas o flores. Cada pieza es cuidadosamente elaborada para resaltar la belleza de la naturaleza, respetando su ciclo de vida y transformándola en una obra de arte portátil.',
    legalInfo: 'Especie producida en cautiverio/Specimens raised in captivity. Permitida su venta según decreto # 40548 MINAE Art. 115 de la ley #7317 de vida silvestre. Programa de zoocriaderos, SINAC-ACC-OA-re–1543-2022',
     socialLinks: {
-      instagram: 'https://www.instagram.com/tu_usuario_instagram',
-      facebook: 'https://www.facebook.com/tu_usuario_facebook',
+      instagram: 'https://www.instagram.com/elizondo6981?utm_source=qr',
+      facebook: 'https://www.facebook.com/people/Guarakay/100064912232991/?mibextid=LQQJ4d&rdid=KON91gO9DehSL7qq&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2FYfdWaRkn5JVL7ZPB%2F%3Fmibextid%3DLQQJ4d',
       whatsapp: 'https://wa.me/50686165808'
     }
   };
